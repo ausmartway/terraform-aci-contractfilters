0de65e115b4477dec6ee5420406f72278d899c48
Yulei Liu <yulei.liu@gmail.com>
2022-02-02 09:37:23 +1100

    first draft

--- main.tf
+++ main.tf
@@ -1,1 +1,17 @@
 ##main.tf
+#HTTP Traffic
+resource "aci_filter" "filter" {
+  for_each  = var.openports
+  name      = "filter-${each.key}"
+  tenant_dn = var.tenant_id
+}
+
+resource "aci_filter_entry" "filter_entry" {
+  for_each    = var.openports
+  filter_dn   = aci_filter.filter[each.key].id
+  name        = each.key
+  ether_t     = "ip"
+  prot        = each.value.protocol
+  d_from_port = each.vaule.from_port
+  d_to_port   = each.vaule.to_port
+}
--- variables.tf
+++ variables.tf
@@ -1,1 +1,19 @@
 # Define your variables in here.
+variable "tenant_id" {
+  description = "the dn of ACI tenant"
+}
+variable "openports" {
+  default = {
+    http = {
+      from_port = "80",
+      to_port   = "80"
+      protocol  = "tcp",
+    },
+    ssh = {
+      from_port = "22",
+      to_port   = "22",
+      protocol  = "tcp",
+    }
+  }
+  description = "List of ports that should be open."
+}
