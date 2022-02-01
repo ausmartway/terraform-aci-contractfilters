45771ade738dac6d8c5293688f1da02575515daa
Yulei Liu <yulei.liu@gmail.com>
2022-02-02 09:51:37 +1100

    update provider version requirements

--- versions.tf
+++ versions.tf
@@ -1,8 +1,12 @@
 ##Add your terraform core and provider version constrains here.
 terraform {
   required_version = ">= 1.0"
-  # required_providers {
-  #   tfe = ">= 0.26"
-  # }
+  
+  required_providers {
+    aci = {
+      source  = "ciscodevnet/aci"
+      version = "~> 2.0"
+    }
+  }
 
 }
